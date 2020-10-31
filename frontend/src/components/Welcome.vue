466e7ef2bd22998c8ede9aba58ea8957e514ae80
Marek Sierociński <mareksierocinski@gmail.com>
2020-10-31 17:49:16 +0100

    feat: Strona powitania i akceptacji ciasteczek

---
+++ frontend/src/components/Welcome.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="page-welcome" :aria-label="i18n('WELCOME_PAGE')">
+    <form>
+      <div class="align-center">
+        <img
+          src="~Img/logo-purple.svg"
+          alt="Logo Omódlmy Net, przedstawiające różaniec oplatający glob, a pośrodku jest krzyż"
+        />
+      </div>
+
+      <h1>Pokój i dobro!</h1>
+
+      <p>
+        Aplikacja <strong>Omódlmy Net</strong> to miejsce, w którym
+        <strong>anonimowo</strong> możesz nadać swoją intencję i/lub wesprzeć
+        innych modlitwą. Nie jest wymagana rejestracja, ale do poprawnego
+        działania aplikacji używane są <strong>ciasteczka</strong> (czyli takie
+        małe pliki trzymane przez przeglądarkę), tak więc korzystanie z niej oznacza
+        zgodę na ich wykorzystywanie.
+        <br />
+        Równocześnie informujemy, że aplikacja w żaden sposób
+        <strong>nie śledzi Ciebie</strong>
+        - nie korzysta technologii wielkich korporacji czy tzw. trackerów.
+        Aplikacja jest w 100% darmowa i bez reklam (co swoją drogą możesz
+        sprawdzić w otwarym kodzie aplikacji!).
+      </p>
+      <p>Pamiętaj, że <strong>Twoja modlitwa się liczy!</strong></p>
+
+      <button
+        type="submit"
+        class="btn"
+        @submit.stop.prevent="submit"
+        @click.stop.prevent="submit">
+        Rozumiem i akceptuję
+      </button>
+
+      <p>
+        P.S. Jeżli widzisz gdzieś błąd, to skorzystaj proszę z opcji
+        <strong>Kontakt</strong> i zgłoś to. Mile widziane też wszelkie sugestie
+        czy dobre słowo :)
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+import Cookies from 'js-cookie';
+
+export default {
+  name: "welcome",
+
+  methods: {
+    submit() {
+      Cookies.set('accepted', 'true', { expires: 3650 });
+      this.$parent.cookies.accepted = true;
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import "~Stylesheets/mixins/responsiveness";
+
+#page-welcome {
+  max-width: 40rem;
+  padding: 1rem;
+  margin-left: auto;
+  margin-right: auto;
+
+  img {
+    width: 20vh;
+    height: auto;
+  }
+
+  p {
+    line-height: 1.5;
+  }
+
+  .btn {
+    margin-top: 2rem;
+    margin-bottom: 4rem;
+  }
+}
+</style>
