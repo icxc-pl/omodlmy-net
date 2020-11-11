63113dd259bdecbdcbf411a4bd018cd352658ad7
Marek Sierociński <mareksierocinski@gmail.com>
2020-11-11 09:52:30 +0100

    test: Testy integracyjne dla Menu głównego

--- test/test/4-integration.js
+++ test/test/4-integration.js
@@ -15,16 +15,28 @@ describe('Integracja', function () {
   this.timeout(5000);
 
 
-  const getText = async (selector) => {
-    const el = await page.$(selector);
-    let text = await page.evaluate(el => el.textContent, el);
+  const getVisible = async (selector) => {
+    return await page.waitForSelector(selector, { visible: true });
+  };
+
+  const getValueOfSomething = async (selector, callback, ...args) => {
+    const el = typeof selector === 'string' ? await getVisible(selector) : selector;
+    args.unshift(el);
+
+    let text = await page.evaluate(callback, ...args);
     return text.trim();
   };
 
+  const getText = async (selector) => {
+    return await getValueOfSomething(selector, el => el.textContent);
+  };
+
+  const getAttr = async (selector, attrName) => {
+    return await getValueOfSomething(selector, (el, attrName) => el.getAttribute(attrName), attrName);
+  };
+
   const getAriaLabelOfCurrentPage = async () => {
-    const el = await page.$('[role="main"]');
-    let val = await page.evaluate(el => el.getAttribute('aria-label'), el);
-    return val.trim();
+    return await getAttr('[role="main"]', 'aria-label');
   };
 
 
@@ -59,6 +71,105 @@ describe('Integracja', function () {
   });
 
 
+  describe('Menu główne', () => {
+
+    const menuElements = [
+      {
+        title: 'Start',
+        to: 'Ekran główny'
+      },
+      {
+        title: 'Lista intencji',
+        to: 'Lista intencji'
+      },
+      {
+        title: 'Nadaj intencję',
+        to: 'Formularz nadawania intencji'
+      },
+      {
+        title: 'Lista moich intencji',
+        to: 'Lista moich intencji'
+      },
+      {
+        title: 'Jak się modlić?',
+        to: 'Jak się modlić?'
+      },
+      {
+        title: 'O aplikacji',
+        to: 'O aplikacji'
+      }
+    ];
+
+
+    it('menu jest zamknięte', async () => {
+      let val = await getAttr('.main-menu-trigger', 'aria-expanded');
+      expect(val).to.be.eq('false');
+
+      val = await getAttr('nav.main-menu', 'class');
+      expect(val).to.not.include('opened');
+    });
+
+    it('widać przycisk "Menu"', async () => {
+      const el = await getVisible('.main-menu-trigger');
+      const text = await getText(el);
+      expect(text).to.be.eq('Menu');
+    });
+
+    it('otwórz menu', async () => {
+      await page.click('.main-menu-trigger');
+    });
+
+    it('menu jest otwarte', async () => {
+      let val = await getAttr('.main-menu-trigger', 'aria-expanded');
+      expect(val).to.be.eq('true');
+
+      val = await getAttr('nav.main-menu', 'class');
+      expect(val).to.include('opened');
+    });
+
+    it('widać przycisk "Zamknij menu"', async () => {
+      const el = await getVisible('.main-menu-trigger');
+      const text = await getText(el);
+      expect(text).to.be.eq('Zamknij menu');
+    });
+
+    it('widać listę', async () => {
+      const el = await getVisible('.main-menu-container > ul[role="menu"]');
+      expect(el).to.be.not.null;
+    });
+
+    for (let i = 0; i < menuElements.length; i++) {
+      ((n, item) => {
+        it(`lista: widać "${item.title}"`, async () => {
+          const el = await getVisible(`.main-menu-container > ul[role="menu"] > li:nth-child(${n})`);
+          expect(el).to.be.not.null;
+
+          const text = await getText(el);
+          expect(text).to.be.eq(item.title);
+        });
+
+        it(`lista: "${item.title}" przenosi poprawnie`, async () => {
+          await page.click(`.main-menu-container > ul[role="menu"] > li:nth-child(${n})`);
+          await sleep(100);
+          const val = await getAriaLabelOfCurrentPage();
+          expect(val).to.be.eq(item.to);
+          await page.click('.main-menu-trigger');
+        });
+      })(i + 1, menuElements[i]);
+    }
+
+    it('widać stopkę', async () => {
+      const el = await page.$('footer[role="contentinfo"]');
+      expect(el).to.be.not.null;
+    });
+
+    it('przejdź na ekran główny (zamknij menu)', async () => {
+      await page.click(`.main-menu-container > ul[role="menu"] > li:nth-child(1)`);
+    });
+
+  });
+
+
   describe('Start', () => {
 
     it('jest widoczna liczba modlitw i dołączeń', async function () {
@@ -66,29 +177,29 @@ describe('Integracja', function () {
 
       await sleep(1000);
       const text = await getText('h3');
-      expect(text).to.have.eq('0 intencji i 0 dołączeń');
+      expect(text).to.be.eq('0 intencji i 0 dołączeń');
     });
 
     it('jest widoczny slogan', async () => {
       const text = await getText('h4');
-      expect(text).to.have.eq('Twoja modlitwa się liczy!');
+      expect(text).to.be.eq('Twoja modlitwa się liczy!');
     });
 
     it('jest widoczny przycisk "Modlę się"', async () => {
       const text = await getText('a.btn.btn-round');
-      expect(text).to.have.eq('Modlę się');
+      expect(text).to.be.eq('Modlę się');
     });
 
     it('jest widoczny przycisk "Nadaj intencję"', async () => {
       const text = await getText('a.btn:not(.btn-round)');
-      expect(text).to.have.eq('Nadaj intencję');
+      expect(text).to.be.eq('Nadaj intencję');
     });
 
     it('przycisk "Modlę się" przenosi na listę intencji', async () => {
       await page.click('a.btn.btn-round');
       await sleep(100);
       const val = await getAriaLabelOfCurrentPage();
-      expect(val).to.have.eq('Lista intencji');
+      expect(val).to.be.eq('Lista intencji');
       await page.goBack();
     });
 
@@ -96,7 +207,7 @@ describe('Integracja', function () {
       await page.click('a.btn:not(.btn-round)');
       await sleep(100);
       const val = await getAriaLabelOfCurrentPage();
-      expect(val).to.have.eq('Formularz nadawania intencji');
+      expect(val).to.be.eq('Formularz nadawania intencji');
       await page.goBack();
     });
 
